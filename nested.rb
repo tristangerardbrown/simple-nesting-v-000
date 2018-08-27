30d4a2c3f193c3d659f9e6f0fd9bc2944937b5f1
Tristan Brown <tgbrown@stanford.edu>
2018-08-27 22:31:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     programmer_hash[:alan_kay] = {:known_for => alans_new_info}
+     programmer_hash[:alan_kay] = {:known_for => "GUI"}
      alans_new_info = "GUI"
 		 programmer_hash
 
